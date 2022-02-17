87ce00231e24a128a2adb01b2544751172e35ede
Abel Cheung <abelcheung@gmail.com>
2022-02-17 05:36:40 +0000

    Redesign GH workflow, move test dependencies to tox

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ import re
 
 from setuptools import setup
 
-tests_require = [
-    "pytest>=6.0.0",
-    "pytest-mypy-plugins==1.4.0",
-    "coverage[toml]==5.2",
-]
-
 with open("README.md") as fh:
     long_description = re.sub(
         "<!-- start-no-pypi -->.*<!-- end-no-pypi -->\n",
@@ -24,8 +18,6 @@ setup(
     long_description_content_type="text/markdown",
     package_data={"lxml-stubs": ["*.pyi", "*/*.pyi"]},
     packages=["lxml-stubs"],
-    tests_require=tests_require,
-    extras_require={"test": tests_require},
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
     ],
