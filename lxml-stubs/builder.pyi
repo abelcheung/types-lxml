e5491079891ff265dc7ddc67a3973abd2a50c5e1
Abel Cheung <abelcheung@gmail.com>
2022-03-07 04:58:05 +0000

    Specialization of ElementMaker

--- lxml-stubs/builder.pyi
+++ lxml-stubs/builder.pyi
@@ -1,17 +1,38 @@
-from typing import Any, Callable, Mapping
+from typing import Any, Callable, Generic, Mapping, TypeVar, overload
 
 from ._types import _NSMapArg
 from .etree import QName, _Element, _ElemFactory
 
-class ElementMaker:
-    def __init__(
-        self,
-        typemap: Mapping[Any, Callable[[_Element, Any], None]] | None = ...,
+_ET_co = TypeVar("_ET_co", bound=_Element, covariant=True)
+
+_TypeMapArg = Mapping[Any, Callable[[_Element, Any], None]]
+
+class ElementMaker(Generic[_ET_co]):
+    @overload
+    def __new__(
+        cls,
+        typemap: _TypeMapArg | None = ...,
         namespace: str | None = ...,
         nsmap: _NSMapArg | None = ...,
-        # same signature as etree.Element()
-        makeelement: _ElemFactory[_Element] | None = ...,
-    ) -> None: ...
+        *,
+        makeelement: _ElemFactory[_ET_co],
+    ) -> ElementMaker[_ET_co]: ...
+    @overload
+    def __new__(
+        cls,
+        typemap: _TypeMapArg | None,
+        namespace: str | None,
+        nsmap: _NSMapArg | None,
+        makeelement: _ElemFactory[_ET_co],
+    ) -> ElementMaker[_ET_co]: ...
+    @overload
+    def __new__(
+        cls,
+        typemap: _TypeMapArg | None = ...,
+        namespace: str | None = ...,
+        nsmap: _NSMapArg | None = ...,
+        makeelement: None = ...,
+    ) -> ElementMaker[_Element]: ...
     def __call__(
         self,
         tag: str | QName,  # No bytes here
@@ -21,11 +42,11 @@ class ElementMaker:
         # type are accepted.
         *children: Any,
         **attrib: str,
-    ) -> _Element: ...
+    ) -> _ET_co: ...
     # __getattr__ here is special. ElementMaker is a factory that generates
     # elements with any tag provided as attribute name, as long as the name
     # does not conflict with the basic object methods (including python keywords
     # like "class" and "for", which are common in HTML)
-    def __getattr__(self, name: str) -> _ElemFactory[_Element]: ...
+    def __getattr__(self, name: str) -> _ElemFactory[_ET_co]: ...
 
-E: ElementMaker
+E: ElementMaker[_Element]
