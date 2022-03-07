621232e7c333f73838e14a06b27d3309cdd9af8e
Krzysztof Jagiello <me@kjagiello.com>
2022-03-07 14:41:56 +0100

    Add typehints for objectify.ElementMaker

--- lxml-stubs/objectify.pyi
+++ lxml-stubs/objectify.pyi
@@ -1,5 +1,7 @@
-from ._types import _AnyStr
-from .etree import ElementBase, XMLParser
+from typing import Any, Callable, Mapping
+
+from ._types import _AnyStr, _NSMapArg
+from .etree import ElementBase, QName, XMLParser, _ElemFactory
 
 class ObjectifiedElement(ElementBase):
     def __getattr__(self, __k: str) -> ObjectifiedElement: ...
@@ -7,3 +9,30 @@ class ObjectifiedElement(ElementBase):
 def fromstring(
     text: _AnyStr, parser: XMLParser = ..., *, base_url: _AnyStr = ...
 ) -> ObjectifiedElement: ...
+
+class ElementMaker:
+    def __init__(
+        self,
+        namespace: str | None = ...,
+        nsmap: _NSMapArg | None = ...,
+        annotate: bool = ...,
+        # same signature as etree.Element()
+        makeelement: _ElemFactory[ObjectifiedElement] | None = ...,
+    ) -> None: ...
+    def __call__(
+        self,
+        tag: str | QName,  # No bytes here
+        # Although, by default, the ElementMaker only accepts _Element and types
+        # interpretable by the default typemap (that is str, CDATA and dict)
+        # as children, the typemap can be expanded to make sure items of any
+        # type are accepted.
+        *children: Any,
+        **attrib: str,
+    ) -> ObjectifiedElement: ...
+    # __getattr__ here is special. ElementMaker is a factory that generates
+    # elements with any tag provided as attribute name, as long as the name
+    # does not conflict with the basic object methods (including python keywords
+    # like "class" and "for", which are common in HTML)
+    def __getattr__(self, name: str) -> _ElemFactory[ObjectifiedElement]: ...
+
+E: ElementMaker
