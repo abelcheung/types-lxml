176be26c8098ea698d3b398ccfabd1fe1f9484ba
Abel Cheung <abelcheung@gmail.com>
2024-02-15 03:13:37 +0000

    test: _Attrib tests migrated to runtime one

---
+++ test-rt/test_attrib.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import copy
+
+import _testutils
+import pytest
+from lxml.etree import _Element, _ElementTree
+
+reveal_type = getattr(_testutils, "reveal_type_wrapper")
+
+
+class TestXmlAttrib:
+    def test_behavior(self, xml_tree: _ElementTree[_Element]) -> None:
+        e = copy.deepcopy(xml_tree.getroot())
+        attrib = e.attrib
+        reveal_type(len(attrib))
+        attrib.update([("foo", "foo"), (b"bar", b"bar")])
+        attrib.update({"foo": "foo", b"bar": b"bar"})
+        for k in attrib:
+            reveal_type(k)
+            reveal_type(attrib[k])
+        attrib[b"bar"] = "foo"
+        del attrib["bar"]
+        reveal_type(attrib.get("bar"))
+        reveal_type(attrib.get("bar", 0))
+        reveal_type(attrib.get("orderid"))
+        reveal_type(attrib.has_key("whatever"))
+        with pytest.raises(TypeError, match="Argument must be bytes or unicode"):
+            attrib["x"] = 1  # type: ignore
+        # Will fail, possibly got evaluated twice
+        # v1 = attrib.pop('orderid')
+        # reveal_type(v1)
+        reveal_type(attrib.pop("whatever", 0))
+        for k in attrib.keys():
+            reveal_type(k)
+        for v in attrib.values():
+            reveal_type(v)
+        for k, v in attrib.items():
+            reveal_type(k)
+            reveal_type(v)
+
+        ik = attrib.iterkeys()
+        reveal_type(ik)
+        for k in ik:
+            reveal_type(k)
+
+        iv = attrib.itervalues()
+        reveal_type(iv)
+        for v in iv:
+            reveal_type(v)
+
+        ii = attrib.iteritems()
+        reveal_type(ii)
+        for i in ii:
+            reveal_type(i)
