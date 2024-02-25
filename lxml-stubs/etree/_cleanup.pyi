07ca611466d9f6f6518530f971e8a6c06688c7b5
Abel Cheung <abelcheung@gmail.com>
2024-02-25 23:03:39 +0000

    fix: First arg of etree.clean_*() funcs must be positional
    Pyright ≥ 1.1.351 produced warning on the signature, which turns out to potentionally indicate incorrect stub.

--- lxml-stubs/etree/_cleanup.pyi
+++ lxml-stubs/etree/_cleanup.pyi
@@ -7,30 +7,40 @@ def cleanup_namespaces(
     top_nsmap: _NSMapArg | None = ...,
     keep_ns_prefixes: Iterable[_AnyStr] | None = ...,
 ) -> None: ...
+
+# For functions below, the first `tree_or_element` argument
+# can never be keyword argument, since tag/attribute names
+# that followed are considered positional arguments in
+# all possible function signature overloads.
+
+@overload
 def strip_attributes(
-    tree_or_element: _ElementOrTree,
+    __tree_or_elem: _ElementOrTree,
     *attribute_names: str,
 ) -> None: ...
 @overload
+def strip_attributes(
+    __tree_or_elem: _ElementOrTree, __attrib: Iterable[str] | None, /
+) -> None: ...
+@overload
 def strip_elements(
-    tree_or_element: _ElementOrTree,
+    __tree_or_elem: _ElementOrTree,
     *tag_names: _TagSelector,
     with_tail: bool = ...,
 ) -> None: ...
 @overload
 def strip_elements(
-    tree_or_element: _ElementOrTree,
+    __tree_or_elem: _ElementOrTree,
     __tag: Iterable[_TagSelector] | None,
     /,
     with_tail: bool = ...,
 ) -> None: ...
 @overload
 def strip_tags(
-    tree_or_element: _ElementOrTree,
+    __tree_or_elem: _ElementOrTree,
     *tag_names: _TagSelector,
 ) -> None: ...
 @overload
 def strip_tags(
-    tree_or_element: _ElementOrTree,
-    __tag: Iterable[_TagSelector] | None,
+    __tree_or_elem: _ElementOrTree, __tag: Iterable[_TagSelector] | None, /
 ) -> None: ...
