7c435a129e71838b954e10177a9fd75d2cf744aa
Abel Cheung <abelcheung@gmail.com>
2024-04-02 17:42:49 +0000

    fix: lxml.ElementInclude completely redone
    - Drop unneeded overload for ElementInclude.include()
    - And fix its return type
    - Use callback protocol for loader arg, Callable is insufficient
    - Docstring on how to use the protocol

--- lxml-stubs/ElementInclude.pyi
+++ lxml-stubs/ElementInclude.pyi
@@ -1,31 +1,58 @@
-from typing import Any, Callable, Literal, overload
+from typing import Any, Literal, Protocol, overload
 
-from lxml.etree import LxmlSyntaxError, _Element, _ElementTree
+from lxml.etree import LxmlSyntaxError, _Element
 
-from ._types import _ET, _FilePath
+from ._types import _ElementOrTree
 
 class FatalIncludeError(LxmlSyntaxError): ...
 class LimitedRecursiveIncludeError(FatalIncludeError): ...
 
-# The only purpose of default_loader is for the
-# 'loader' arg in include() below, so
-# accurate @overload is unneeded
-def default_loader(
-    href: _FilePath,
-    parse: Literal["xml", "text"],
-    encoding: str | None = None,
-) -> _ElementTree[_Element] | str: ...
-@overload
+# The default_loader() in lxml.ElementInclude is completely
+# retired (lxml uses its own internal loader)
+
+class LoaderProtocol(Protocol):
+    """Protocol for loader func argument in `ElementInclude.include()`
+
+    Annotation
+    ----------
+    `loader=` argument in `ElementInclude.include()` specifies the function
+    object to load URL or file resource. It has the following overloaded
+    function signature:
+    - `(_href: str, _mode: Literal["xml"], /, encoding: str = None) -> _Element`
+    - `(_href: str, _mode: Literal["text"], /, encoding: str = None) -> str`
+    """
+
+    @overload
+    def __call__(
+        self,
+        _href: str,  # URL or local path from href="..." attribute
+        _mode: Literal["xml"],
+        /,
+        encoding: Any = None,  # Under XML mode this param is ignored
+                               # but must be present nontheless
+    ) -> _Element: ...
+    @overload
+    def __call__(
+        self,
+        _href: str,
+        _mode: Literal["text"],
+        /,
+        encoding: str | None = None,
+    ) -> str: ...
+
 def include(
-    elem: _ET,
-    loader: Callable[[_FilePath, Literal["xml", "text"]], Any] | None = None,
+    elem: _ElementOrTree,
+    loader: LoaderProtocol | None = None,
     base_url: str | None = None,
     max_depth: int = 6,
-) -> _ET: ...
-@overload
-def include(
-    elem: _ElementTree[_ET],
-    loader: Callable[[_FilePath, Literal["xml", "text"]], Any] | None = None,
-    base_url: str | None = None,
-    max_depth: int = 6,
-) -> _ElementTree[_ET]: ...
+) -> None:
+    """Expand XInclude directives
+
+    Annotation
+    ----------
+    - Source documentation above `include()` is outdated; this function
+    does not return at all.
+    - Try using `from lxml.ElementInclude import LoaderProtocol` from
+    within IDEs to lookup its purpose and usage. This is annotation
+    only and doesn't exist in lxml source.
+    """
