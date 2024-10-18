3ca09081d657abd0618e4f68f363510a89d4b38b
Abel Cheung <abelcheung@gmail.com>
2024-10-18 19:10:15 +0000

    fix: Multiple fixes and enhancements in Namespace Registry
    - Decorator signatures were incorrect, first level wrapped function shouldn't have a 2nd parameter
    - etree.FunctionNamespace() should have produced a _XPathFunctionNamespaceRegistry, not _FunctionNamespaceRegistry
    - No more inherit from MutableMapping now
    - _NamespaceRegistry merged into its 2 subclasses for easier maintenance
    - Add docstrings for FunctionNamespace() and various update() methods

--- lxml-stubs/etree/_nsclasses.pyi
+++ lxml-stubs/etree/_nsclasses.pyi
@@ -4,7 +4,6 @@ from typing import (
     Callable,
     Iterable,
     Iterator,
-    MutableMapping,
     TypeVar,
     final,
     overload,
@@ -20,10 +19,8 @@ from ._classlookup import ElementBase, ElementClassLookup, FallbackElementClassL
 from ._module_misc import LxmlError
 
 _T = TypeVar("_T")
-_KT = TypeVar("_KT")
-_VT = TypeVar("_VT")
 _P = ParamSpec("_P")
-_Public_ET = TypeVar("_Public_ET", bound=ElementBase)
+_Public_ET = TypeVar("_Public_ET", bound=type[ElementBase])
 
 class LxmlRegistryError(LxmlError):
     """Base class of lxml registry errors"""
@@ -31,34 +28,41 @@ class LxmlRegistryError(LxmlError):
 class NamespaceRegistryError(LxmlRegistryError):
     """Error registering a namespace extension"""
 
-# Yet another dict-wannabe that lacks many normal methods and add a few
-# of its own
-class _NamespaceRegistry(MutableMapping[_KT, _VT]):
-    def __delitem__(self, __key: _KT) -> None: ...
-    def __getitem__(self, __key: _KT) -> _VT: ...
-    def __setitem__(self, __key: _KT, __value: _VT) -> None: ...
-    def __iter__(self) -> Iterator[_KT]: ...
-    def __len__(self) -> int: ...
-    def update(  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]
-        self,
-        class_dict_iterable: SupportsLaxedItems[_KT, _VT] | Iterable[tuple[_KT, _VT]],
-    ) -> None: ...
-    def items(self) -> list[tuple[_KT, _VT]]: ...  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]
-    def iteritems(self) -> Iterator[tuple[_KT, _VT]]: ...
-    def clear(self) -> None: ...
-
 #
 # Element namespace
 #
 
+# Incorporated the generic dict-like _NamespaceRegistry
 @final
-class _ClassNamespaceRegistry(_NamespaceRegistry[str | None, type[ElementBase]]):
+class _ClassNamespaceRegistry:
+    "Dictionary-like registry for namespace implementation classes"
+    def __delitem__(self, __key: str | None) -> None: ...
+    def __getitem__(self, __key: str | None) -> type[ElementBase]: ...
+    def __setitem__(self, __key: str | None, __value: type[ElementBase]) -> None: ...
+    def __iter__(self) -> Iterator[str | None]: ...
+    def __len__(self) -> int: ...
+    def update(
+        self,
+        class_dict_iterable: SupportsLaxedItems[str | None, type[ElementBase]]
+        | Iterable[tuple[str | None, type[ElementBase]]],
+    ) -> None:
+        """Forgivingly update the registry.
+
+        ``class_dict_iterable`` may be a dict or some other iterable
+        that yields (name, value) pairs.
+
+        If a value does not match the required type for this registry,
+        or if the name starts with '_', it will be silently discarded.
+        This allows registrations at the module or class level using
+        ``vars()``, ``globals()`` etc.
+        """
+    def items(self) -> list[tuple[str | None, type[ElementBase]]]: ...
+    def iteritems(self) -> Iterator[tuple[str | None, type[ElementBase]]]: ...
+    def clear(self) -> None: ...
     @overload  # @ns(None), @ns('tag')
-    def __call__(
-        self, __tag: str | None, __cls: None = None
-    ) -> Callable[[type[_Public_ET]], type[_Public_ET]]: ...
+    def __call__(self, _tag: str | None, /) -> Callable[[_Public_ET], _Public_ET]: ...
     @overload  # plain @ns
-    def __call__(self, __cls: type[_Public_ET]) -> type[_Public_ET]: ...
+    def __call__(self, obj: _Public_ET, /) -> _Public_ET: ...
 
 class ElementNamespaceClassLookup(FallbackElementClassLookup):
     """Element class lookup scheme that searches the Element class in the
@@ -94,17 +98,66 @@ class ElementNamespaceClassLookup(FallbackElementClassLookup):
 # Function namespace
 #
 
+# Incorporated the generic dict-like _NamespaceRegistry
 @final
-class _FunctionNamespaceRegistry(_NamespaceRegistry[str, Callable[..., Any]]):
-    @property
-    def prefix(self) -> str: ...
-    @prefix.setter
-    def prefix(self, __v: str | None) -> None: ...
+class _XPathFunctionNamespaceRegistry:
+    # At runtime it is possible to unset prefix by setting the
+    # value to None, but mypy wants to murder me if getter
+    # and setter variable types are different. For everywhere
+    # else the death threat is ignored; but here it is possible
+    # to just del .prefix as alternative, so we have wiggle
+    # room to spare and let mypy has its way.
+    prefix: str
+
+    def __delitem__(self, __key: str) -> None: ...
+    def __getitem__(self, __key: str) -> Callable[..., Any]: ...
+    def __setitem__(self, __key: str, __value: Callable[..., Any]) -> None: ...
+    def __iter__(self) -> Iterator[str]: ...
+    def __len__(self) -> int: ...
+    def update(
+        self,
+        class_dict_iterable: SupportsLaxedItems[str, Callable[..., Any]]
+        | Iterable[tuple[str, Callable[..., Any]]],
+    ) -> None:
+        """Forgivingly update the registry.
+
+        ``class_dict_iterable`` may be a dict or some other iterable
+        that yields (name, value) pairs.
+
+        If a value does not match the required type for this registry,
+        or if the name starts with '_', it will be silently discarded.
+        This allows registrations at the module or class level using
+        ``vars()``, ``globals()`` etc.
+        """
+    def items(self) -> list[tuple[str, Callable[..., Any]]]: ...
+    def iteritems(self) -> Iterator[tuple[str, Callable[..., Any]]]: ...
+    def clear(self) -> None: ...
     @overload  # @ns('name')
     def __call__(
-        self, __name: str, __func: None = None
+        self, _funcname: str, /
     ) -> Callable[[Callable[_P, _T]], Callable[_P, _T]]: ...
     @overload  # plain @ns
-    def __call__(self, __func: Callable[_P, _T]) -> Callable[_P, _T]: ...
+    def __call__(self, obj: Callable[_P, _T], /) -> Callable[_P, _T]: ...
 
-def FunctionNamespace(ns_uri: str | None) -> _FunctionNamespaceRegistry: ...
+def FunctionNamespace(ns_uri: str | None) -> _XPathFunctionNamespaceRegistry:
+    """Retrieve the function namespace object associated with the given
+    URI
+
+    Creates a new one if it does not yet exist. A function namespace
+    can only be used to register extension functions.
+
+    Usage
+    -----
+
+    ```python
+    >>> ns_functions = FunctionNamespace("http://schema.org/Movie")
+
+    >>> @ns_functions  # uses function name
+    ... def add2(x):
+    ...     return x + 2
+
+    >>> @ns_functions("add3")  # uses explicit name
+    ... def add_three(x):
+    ...     return x + 3
+    ```
+    """
