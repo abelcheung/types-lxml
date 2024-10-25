f565104a11d6226cdd3ec620c6711525cc1f0d24
Abel Cheung <abelcheung@gmail.com>
2024-10-25 21:47:35 +0000

    fix(mypy): Stub passes type check with 1.10
    Runtime check still requires mypy 1.11 however

--- lxml-stubs/html/_parse.pyi
+++ lxml-stubs/html/_parse.pyi
@@ -91,7 +91,7 @@ def document_fromstring(
     base_url: str | None = None,
 ) -> HtmlElement: ...
 @overload
-def fragments_fromstring(  # pyright: ignore[reportOverlappingOverload]
+def fragments_fromstring(  # type: ignore[overload-overlap]  # pyright: ignore[reportOverlappingOverload]
     html: _AnyStr,
     no_leading_text: Literal[True],
     base_url: str | None = None,
