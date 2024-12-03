40572825d92f254f59c798b3199885ef7d9ff555
Abel Cheung <abelcheung@gmail.com>
2024-12-03 09:48:41 +0000

    test(pytest): Add element fixture with <base href="...">
    Some HTTP link functions really need that to work

--- tests/runtime/html/conftest.py
+++ tests/runtime/html/conftest.py
@@ -7,3 +7,19 @@ from lxml.html import Element, HtmlElement
 @pytest.fixture(scope="class")
 def disposable_html_element() -> HtmlElement:
     return Element("div", {"class": "disposable"})
+
+
+# HTML link replacement functions need <base href="...">,
+# otherwise they short circuit and ignore invalid args
+@pytest.fixture(scope="class")
+def disposable_html_with_base_href() -> HtmlElement:
+    import lxml.html.builder as b
+    return b.HTML(
+        b.HEAD(
+            # urljoin is too robust (?)
+            b.BASE(href="?foo?<")
+        ),
+        b.BODY(
+            b.A("Link text", href="#id1", id="id1"),
+        )
+    )
