bded18ccc29e5b032828f06011d758afbc1b0eb1
Abel Cheung <abelcheung@gmail.com>
2025-04-10 17:38:20 +0000

    fix(stub): cleanup_namespaces() shouldn't warn without keep_ns_prefixes arg
    Closes #85

--- src/lxml-stubs/etree/_cleanup.pyi
+++ src/lxml-stubs/etree/_cleanup.pyi
@@ -18,15 +18,58 @@ from .._types import (
 @deprecated("Supply an iterable of namespace prefixes, instead of a vanilla prefix.")
 def cleanup_namespaces(
     tree_or_element: _ElementOrTree,
+    top_nsmap: _NSMapArg | None,
+    keep_ns_prefixes: _TextArg,
+) -> None:
+    """Remove all namespace declarations from a subtree that are not used by any
+    of the elements or attributes in that tree.
+
+    Annotation
+    ----------
+    This overload guards against using a single namespace prefix string in
+    `keep_ns_prefixes` argument. Otherwise, the string is split into
+    individual characters and each of them is treated as a namespace prefix.
+
+    See Also
+    --------
+    - [API Documentation](https://lxml.de/apidoc/lxml.etree.html#lxml.etree.cleanup_namespaces)
+    """
+
+@overload
+@deprecated("Supply an iterable of namespace prefixes, instead of a vanilla prefix.")
+def cleanup_namespaces(
+    tree_or_element: _ElementOrTree,
     top_nsmap: _NSMapArg | None = None,
-    keep_ns_prefixes: _TextArg | None = None,
-) -> None: ...
+    *,
+    keep_ns_prefixes: _TextArg,
+) -> None:
+    """Remove all namespace declarations from a subtree that are not used by any
+    of the elements or attributes in that tree.
+
+    Annotation
+    ----------
+    This overload guards against using a single namespace prefix string in
+    `keep_ns_prefixes` argument. Otherwise, the string is split into
+    individual characters and each of them is treated as a namespace prefix.
+
+    See Also
+    --------
+    - [API Documentation](https://lxml.de/apidoc/lxml.etree.html#lxml.etree.cleanup_namespaces)
+    """
+
 @overload
 def cleanup_namespaces(
     tree_or_element: _ElementOrTree,
     top_nsmap: _NSMapArg | None = None,
     keep_ns_prefixes: Iterable[_TextArg] | None = None,
-) -> None: ...
+) -> None:
+    """Remove all namespace declarations from a subtree that are not used by any
+    of the elements or attributes in that tree.
+
+    See Also
+    --------
+    - [API Documentation](https://lxml.de/apidoc/lxml.etree.html#lxml.etree.cleanup_namespaces)
+    """
 
 # For functions below, the first `tree_or_element` argument
 # can never be keyword argument, since tag/attribute names
